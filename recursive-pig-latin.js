a1862d891fb47d274b7b4605618d3a61461a1488
imorenodev <imorenodev@gmail.com>
2016-01-15 21:39:20 -0500

    added comments

--- recursive-pig-latin.js
+++ recursive-pig-latin.js
@@ -1,22 +1,22 @@
 function translate(str) {
-  if (isVowel(str.charAt(0)) >= 0) {
+  if (isVowel(str.charAt(0)) >= 0) { //check is word starts with vowel
     return str.concat("way");
   }
   
-  return pigLatinize(str);
+  return pigLatinize(str); //return recursive function
   
   function pigLatinize(word) {
-    if (isVowel(word.charAt(0)) >= 0) {
+    if (isVowel(word.charAt(0)) >= 0) { //first letter is a vowel
       return word.concat("ay");
     }
-    var firstLetter = word.slice(0, 1);
-    word = word.slice(1).concat(firstLetter);
-    return pigLatinize(word);
+    var firstLetter = word.slice(0, 1); //save current first letter
+    word = word.slice(1).concat(firstLetter); //move first letter to end of word
+    return pigLatinize(word); //recurse
   }
   
   function isVowel(letter) {
     var vowels = ["a", "e", "i", "o", "u"];
-    return vowels.indexOf(letter.toLowerCase());
+    return vowels.indexOf(letter.toLowerCase()); //if consonant return -1
   }
 }
 
